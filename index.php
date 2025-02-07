3a7a9b7cbfc697e638f38d560882a52ff25123a2
Downzin01 <downmestre@gmail.com>
2025-02-07 11:34:50 -0300

    :sparkles: Arquivo principal do cinebox

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    require './classes/Filmes.php';
+
+    $titulo = 'CineBox - Início';
+    include './includes/header.php';
+    include './includes/banner.php';
+    
+    $filmes = new Filmes();
+    $dados = $filmes->listarFilmesBanco();
+    echo $dados;
+    
+    include './includes/filme_lista.php';
+
+    include './includes/footer.php';
